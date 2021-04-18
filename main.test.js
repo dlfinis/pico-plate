d817f1ed8b57c5aa8eda4ddab83a37f18e88c16c
Diego Leon <dln.finis@gmail.com>
2021-04-18 18:22:04 -0500

    Initial State

---
+++ main.test.js
@@ -0,0 +1,11 @@
+const main = require('./main');
+
+describe('main', () => {
+   test('test base', () => {
+      expect(true).toBe(true);
+   });
+
+   test('should show values for process', () => {
+      main.
+   });
+});
