462ee56e210ccc86f7ce74c02c42c21abfda2a0c
Diego Leon <dleon@ec.krugercorp.com>
2021-04-18 23:40:11 -0500

    Add methods for check plate and date.

--- main.js
+++ main.js
@@ -6,26 +6,105 @@
 'use strict';
 const inputData = require('./inputData');
 
+function isValidValue(typeData, value) {
+    // console.log('isValidValue', typeData, value);
+    if (typeof typeData !== 'function') {
+        throw new Error('Not valid class of data type', typeData);
+    }
+    if (typeof value == 'undefined' || value == null || !value instanceof typeData) {
+        throw new Error('Not valid value', typeData, value);
+    } else {
+        return true;
+    }
+}
+
+function getDateWithTimeRange(dateProcess, timeRange) {
+    if (isValidValue('Date', dateProcess)) {
+        throw new Error('Not valid dateProcess');
+    }
+    if (isValidValue('string', timeRange)) {
+        throw new Error('Not valid timeRange');
+    }
+
+    let dateSet = new Date();
+    Object.assign(dateSet, dateProcess);
+    dateSet.setUTCHours(timeRange.substring(0, 2), timeRange.substring(3));
+    return dateSet;
+}
+
+function isWeekend(dateProcess) {
+    const day = dateProcess.getDay();
+    if (day && day == 5 || day == 6) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isPlateNumberNotBlock(dateProcess, plate) {
+    const controlRestriction = new Map([
+        [0, [1, 2]],
+        [1, [3, 4]],
+        [2, [5, 6]],
+        [3, [7, 8]],
+        [4, [9, 0]],
+    ]);
+    if (isValidValue(Date, dateProcess) && isValidValue(String, plate)) {
+        const stringLength = plate.length;
+        const lastDigit = Number.parseInt(plate.charAt(stringLength - 1));
+        const day = dateProcess.getDay();
+
+        const dayControl = controlRestriction.get(day) || [];
+        const existBlock = dayControl.some(item => item == lastDigit);
+        console.log('day number:',day,'lastDigit: ', lastDigit, 'dayControl:', dayControl, 'existblock:', existBlock);
+        if (dayControl && existBlock) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+}
+
+function validDate(dateProcess, timeRangeMin, timeRangeMax) {
+    // 6:30 a.m. a 10:00 a.m. y de 5:00 p.m. a 9:00 p.m
+    if (isValidValue(Date, dateProcess)) {
+        throw new Error('Not valid dateProcess');
+    }
+
+    if (dateProcess.getUTCHours() > 12) {
+        getDateWithTimeRange(currentDate,)
+    }
+    const dateMin = dateProcess;
+
+}
+
+function validatePlate(dateProcess) {
+    if (isValidValue(Date, dateProcess)) {
+        throw new Error('Not valid dateProcess');
+    }
+
+    if (isWeekend(dateProcess)) {
+        return 'Valid, is weekend';
+    }
+
+    if (isPlateNumberNotBlock(dateProcess)) {
+        return 'Valid, not exist block for driving';
+    }
+}
+
 async function process() {
     console.log('Init process');
-    const controlRestriction = new Map([
-        [1, [0,1]],
-        [2, [2,3]],
-        [3, [4,5]],
-        [4, [6,7]],
-        [5, [8,9]],
-    ]);
+
 
     const dataProcess = await inputData.getArgValues();
-
-    console.log('Data to process', dataProcess, controlRestriction);
 
     const dataDay = new Date(dataProcess.day);
     const dataCheck = dataDay;
     const dataValid = dataDay;
-    dataCheck.setUTCHours(dataProcess.time.substring(0,2),dataProcess.time.substring(3));
-    console.log('Day', dataDay,dataProcess.time.substring(0,2),dataProcess.time.substring(3));
+    dataCheck.setUTCHours(dataProcess.time.substring(0, 2), dataProcess.time.substring(3));
+    console.log('Day', dataDay, dataProcess.time.substring(0, 2), dataProcess.time.substring(3));
     return dataCheck;
 }
 
-module.exports = {process};
+module.exports = {process, isValidValue, isPlateNumberBlock: isPlateNumberNotBlock, getDateWithTimeRange, validatePlate};
--- main.test.js
+++ main.test.js
@@ -1,6 +1,5 @@
 const main = require('./main');
 const inputData = require('./inputData');
-// const jest = require('jest');
 
 describe('main', () => {
    test('test base', () => {
@@ -10,8 +9,40 @@ describe('main', () => {
    test('should show values for process', () => {
       inputData.getArgValues = jest.fn().mockReturnValue({ plate: 'GYU-1234', day: '2021-04-19', time: '12:30' });
       const data = main.process();
-      console.log('data', data);
-      expect(true).toBe(true);
-      // main.inputData() = jest.fn().mockReturnValue(20);
+      expect(data).not.toBeNull();
+   });
+
+   // test('should have a date with a defined time', () => {
+   //    const currentDate = new Date();
+   //    const dateProcess = main.getDateWithTimeRange(currentDate,'13:25');
+   //    console.log('Date to process', currentDate);
+   //    console.log('Date process', dateProcess);
+   //    expect(dateProcess.getUTCHours()).toBe(13);
+   // });
+
+   test('should valid if is a Date', () => {
+      const currentDate = new Date();
+      console.log('typeof Date', typeof currentDate);
+      console.log('instanceof Date', currentDate instanceof Date);
+      const validValue = main.isValidValue(Date, currentDate);
+      expect(validValue).toBe(true);
+   });
+
+   test('should valid value if is a string', () => {
+      const value = 'dataString'
+      console.log('typeof String', typeof String);
+      console.log('typeof String', typeof value);
+      console.log('instanceof String', value instanceof String);
+      const validValue = main.isValidValue(String, value);
+      expect(validValue).toBe(true);
+   });
+
+   test('should valid plate in the day', () => {
+      const process = main.isPlateNumberBlock(new Date('2021-04-15'), 'GYU-1234');
+      expect(process).toBe(true);
+   });
+   test('should not valid plate in the day', () => {
+      const process = main.isPlateNumberBlock(new Date('2021-04-16'), 'GYU-1230');
+      expect(process).toBe(false);
    });
 });
