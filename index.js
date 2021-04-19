1b1e988ec2fef08aef4a9e30d667c7f411e17129
Diego Leon <dleon@ec.krugercorp.com>
2021-04-19 04:01:08 -0500

    Review order index.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const main = require('./main')
 const environment = process.env.NODE_ENV || 'dev';
+const main = require('./main')
+
 if (environment === 'prod') {
     console.log = function () {};
 }
+
 main.processAsync();
